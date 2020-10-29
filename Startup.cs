7d19e911bb2fee0c3d0f4dc8038cca4835525654
Ermogenes Palacio <ermogenes@gmail.com>
2020-10-29 18:00:46 -0300

    Limpa comentários e desativa https redir

--- Startup.cs
+++ Startup.cs
@@ -22,21 +22,17 @@ namespace boardgames_web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
